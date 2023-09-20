ae423c8dbdd6f192bf84871f467e666d28ffd007
VOZ <voisfce@gmail.com>
2023-09-20 16:47:53 +0200

    ajuste python

--- PAP2324/python/test_mymath.py
+++ PAP2324/python/test_mymath.py
@@ -9,28 +9,7 @@ class TestMyMath(unittest.TestCase):
         self.assertEqual(True,mymath.estPair(0))
         self.assertRaises(AssertionError,mymath.estPair,-1)
         self.assertRaises(AssertionError,mymath.estPair,5.0)
-        self.assertRaises(AssertionError,mymath.estPair,"brol")
-    
-    def test_fibo(self):
-        self.assertEqual(0,mymath.fibo(0))
-        self.assertEqual(1,mymath.fibo(1))
-        self.assertEqual(1,mymath.fibo(2))
-        self.assertEqual(8,mymath.fibo(6))
-        self.assertRaises(AssertionError,mymath.fibo,-1)
-    
-    def test_nbBitsV2Bis(self):
-        self.assertEqual(0,mymath.nbBitsV2Bis(0))
-        self.assertEqual(32,mymath.nbBitsV2Bis(-1))
-        self.assertEqual(2,mymath.nbBitsV2Bis(10))
-        self.assertEqual(1,mymath.nbBitsV2Bis(1))
-        
-    def test_nbBitsV1(self):
-        self.assertEqual(0,mymath.nbBitsV1(0))
-        self.assertEqual(2,mymath.nbBitsV1(-1))
-        self.assertEqual(2,mymath.nbBitsV1(10))
-        self.assertEqual(5,mymath.nbBitsV1(-41))
-        self.assertEqual(3,mymath.nbBitsV1(41))
-        
+        self.assertRaises(AssertionError,mymath.estPair,"brol")        
 
 if __name__ == "__main__":
     unittest.main()
