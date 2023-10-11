b2ef616c804e0f2f105fdf44b558c75bc4274586
VOZ <voisfce@gmail.com>
2023-10-11 16:40:45 +0200

    Base Java tp00.Eval

---
+++ PAP2324/src/tp00/Evaluation.java
@@ -0,0 +1,32 @@
+package tp00;
+
+import java.util.Scanner;
+
+public class Evaluation {
+	/**
+	 * Evalue la note pour attribuer une décision
+	 * 0<=note<=100
+	 * @param note une note entre 0 et 100
+	 * @return la décision
+	 */
+	private static String calculDecision(double note) {
+		assert 0<=note && note<=100 : "La note doit être entre 0 et 100";
+		String res;
+		res="A FAIRE";
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Entrez une note: ");
+		int note = scan.nextInt();
+		
+		String decision = calculDecision(note);
+		System.out.println("Hello ta note est: " + note + " la décision est: " + decision);
+		scan.close();
+	}
+
+	
+
+}
