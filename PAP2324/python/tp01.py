3897ccdf293bc0447d13659bd99e9a2cb4c155e3
VOZ <voisfce@gmail.com>
2023-10-11 21:58:07 +0200

    version Python et ajuste java en public

---
+++ PAP2324/python/tp01.py
@@ -0,0 +1,26 @@
+import sys
+
+def calculDecision(note : float)-> str:
+   '''
+    Détermine la décision sur base de la note
+    '''
+    assert 0<=note<=100 ; "La note doit être entre 0 et 100"
+    if note < 30:
+        res = "Raté"
+    elif note<50:
+        res = "Ajourné"
+    else:
+        res = "Réussite"
+    return res
+if __name__=="__main__":
+    #Lecteur de la donnée
+    n = int(input("Entrez un nombre entre 0 et 100: "))
+    while n<0 or n>100:
+        print("ERREUR",file = sys.stderr)
+        n = int(input("Entrez un nombre entre 0 et 100: "))
+
+    #Traitement
+    res = calculDecision(n)
+        
+    #Ecriture du résultat sur la console
+    print(f'votre note {n} donne une décision: {res}')
--- PAP2324/src/tp00/Evaluation.java
+++ PAP2324/src/tp00/Evaluation.java
@@ -3,13 +3,14 @@ package tp00;
 import java.util.Scanner;
 
 public class Evaluation {
+	
 	/**
 	 * Evalue la note pour attribuer une décision
 	 * 0<=note<=100
 	 * @param note une note entre 0 et 100
 	 * @return la décision
 	 */
-	private static String calculDecision(double note) {
+	public static String calculDecision(double note) {
 		assert 0<=note && note<=100 : "La note doit être entre 0 et 100";
 		String res;
 		res="A FAIRE";
