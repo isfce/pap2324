55bd10ff50ddd889a176998f9b8ffb21b5b4b947
VOZ <voisfce@gmail.com>
2023-10-18 14:13:50 +0200

    ajout do while

--- PAP2324/src/tp00/Evaluation.java
+++ PAP2324/src/tp00/Evaluation.java
@@ -3,31 +3,40 @@ package tp00;
 import java.util.Scanner;
 
 public class Evaluation {
-	
+
 	/**
-	 * Evalue la note pour attribuer une décision
-	 * 0<=note<=100
+	 * Evalue la note pour attribuer une décision 0<=note<=100
+	 * 
 	 * @param note une note entre 0 et 100
 	 * @return la décision
 	 */
 	public static String calculDecision(double note) {
-		assert 0<=note && note<=100 : "La note doit être entre 0 et 100";
-		String res;
-		res="A FAIRE";
-		return res;
-	}
-	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.print("Entrez une note: ");
-		int note = scan.nextInt();
-		
-		String decision = calculDecision(note);
-		System.out.println("Hello ta note est: " + note + " la décision est: " + decision);
-		scan.close();
+		assert 0 <= note && note <= 100 : "La note doit être entre 0 et 100";
+		if (note < 30)
+			return "Refus";
+		if (note < 50)
+			return "Ajourné";
+		return "Réussite";
 	}
 
-	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		boolean erreur;
+		int note;
+		// Input
+		do {
+			System.out.print("Entrez une note: ");
+			note = scan.nextInt();
+			erreur = !(note >= 0 && note <= 100);
+			if (erreur)
+				System.err.println("ERREUR");
+		} while (erreur);
+		// traitement
+		String decision = calculDecision(note);
+		// Output
+		System.out.println("Hello ta note est: " + note + " la décision est: " + decision);
+
+		scan.close();
+	}
 
 }
